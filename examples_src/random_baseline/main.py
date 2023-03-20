bc5a451650e165f792b7673dd22963284f63d890
ejm714 <emily@drivendata.org>
2023-03-20 16:03:01 -0700

    use seed

--- examples_src/random_baseline/main.py
+++ examples_src/random_baseline/main.py
@@ -7,14 +7,17 @@ import pandas as pd
 DATA_ROOT = Path("/code_execution/data")
 
 
-def predict(filename):
+def predict(filename, rng):
     """Generate a random probability between 0 and 1."""
-    return np.random.random()
+    return rng.random()
 
 
-def main():
+def main(seed=77):
     # load sumission format
     submission_format = pd.read_csv(DATA_ROOT / "submission_format.csv", index_col=0)
+
+    # set starting random state for consistent runs
+    rng = np.random.RandomState(seed)
 
     # iterate over all images in the metdata
     for filename in submission_format.index:
@@ -26,7 +29,7 @@ def main():
         assert filepath.exists()
 
         # generate a prediction for that file
-        pred = predict(filepath)
+        pred = predict(filepath, rng=rng)
 
         # assign to the right row in the submission format
         submission_format.loc[filename, "relapse"] = pred
